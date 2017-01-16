a21e2bf1d7bbcfe292477b273d98a98adc8453ba
Maks3w <github.maks3w@virtualplanets.net>
2017-01-16 17:08:49 +0100

    Fix HistoricalOrder

--- aplazame/lib/Aplazame/Aplazame/Api/BusinessModel/HistoricalOrder.php
+++ aplazame/lib/Aplazame/Aplazame/Api/BusinessModel/HistoricalOrder.php
@@ -11,6 +11,7 @@ class Aplazame_Aplazame_Api_BusinessModel_HistoricalOrder
 {
     public static function createFromOrder(Order $order)
     {
+        $cart = new Cart($order->id_cart);
         $currency = new Currency($order->id_currency);
         $status = $order->getCurrentStateFull(Context::getContext()->language->id);
 
@@ -23,7 +24,7 @@ class Aplazame_Aplazame_Api_BusinessModel_HistoricalOrder
             'order_date' => Aplazame_Sdk_Serializer_Date::fromDateTime(new DateTime($order->date_add)),
             'currency' => $currency->iso_code,
             'billing' => Aplazame_Aplazame_BusinessModel_Address::createFromAddress(new Address($order->id_address_invoice)),
-            'shipping' => Aplazame_Aplazame_BusinessModel_ShippingInfo::createFromCart($order),
+            'shipping' => Aplazame_Aplazame_BusinessModel_ShippingInfo::createFromCart($cart),
         );
 
         return $serialized;
