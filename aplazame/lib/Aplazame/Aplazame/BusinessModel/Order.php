6559805588cacb88ac70f3b863fbfed48a425f25
Alejandro Hernández <alejandro.herord@gmail.com>
2019-02-05 18:16:07 +0100

    [ADD] 'tax_rate' parameter to 'order' and 'shipping'

--- aplazame/lib/Aplazame/Aplazame/BusinessModel/Order.php
+++ aplazame/lib/Aplazame/Aplazame/BusinessModel/Order.php
@@ -16,10 +16,19 @@ class Aplazame_Aplazame_BusinessModel_Order
     {
         $currency = new Currency($cart->id_currency);
 
+        $total_shipping = $cart->getTotalShippingCost();
+        $total_shipping_tax_exc = $cart->getTotalShippingCost(null, false);
+        $shipping_tax = $total_shipping - $total_shipping_tax_exc;
+
+        $base_total_tax_inc = $cart->getOrderTotal(true);
+        $base_total_tax_exc = $cart->getOrderTotal(false);
+        $order_tax = $base_total_tax_inc - $base_total_tax_exc - $shipping_tax;
+
         $aOrder = new self();
         $aOrder->id = $cart->id;
         $aOrder->currency = $currency->iso_code;
-        $aOrder->total_amount = Aplazame_Sdk_Serializer_Decimal::fromFloat($cart->getOrderTotal(true));
+        $aOrder->total_amount = Aplazame_Sdk_Serializer_Decimal::fromFloat($base_total_tax_inc);
+        $aOrder->tax_rate = Aplazame_Sdk_Serializer_Decimal::fromFloat(100 * $order_tax / $base_total_tax_exc);
         $aOrder->articles = array_map(array('Aplazame_Aplazame_BusinessModel_Article', 'crateFromProductData'), $cart->getProducts());
         $aOrder->discount = Aplazame_Sdk_Serializer_Decimal::fromFloat($cart->getOrderTotal(true, Cart::ONLY_DISCOUNTS));
 
