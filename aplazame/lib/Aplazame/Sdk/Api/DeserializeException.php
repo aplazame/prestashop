b16562b16a5b601f7f1f02aeb9ab1c7c21babeea
Alejandro Hernández <alejandro.herord@gmail.com>
2022-05-03 16:33:33 +0200

    Update copyright

--- aplazame/lib/Aplazame/Sdk/Api/DeserializeException.php
+++ aplazame/lib/Aplazame/Sdk/Api/DeserializeException.php
@@ -3,7 +3,7 @@
  * This file is part of the official Aplazame module for PrestaShop.
  *
  * @author    Aplazame <soporte@aplazame.com>
- * @copyright 2015-2021 Aplazame
+ * @copyright 2015-2022 Aplazame
  * @license   see file: LICENSE
  */
 
