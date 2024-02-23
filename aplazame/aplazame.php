ff0b280a896caab4498d9d089f122cf7054e9d6e
Alejandro Hernández <alejandro.herord@gmail.com>
2024-02-23 12:34:50 +0100

    Release v8.0.0

--- aplazame/aplazame.php
+++ aplazame/aplazame.php
@@ -3,7 +3,7 @@
  * This file is part of the official Aplazame module for PrestaShop.
  *
  * @author    Aplazame <soporte@aplazame.com>
- * @copyright 2015-2023 Aplazame
+ * @copyright 2015-2024 Aplazame
  * @license   see file: LICENSE
  */
 
@@ -46,7 +46,7 @@ class Aplazame extends PaymentModule
     {
         $this->name = 'aplazame';
         $this->tab = 'payments_gateways';
-        $this->version = '7.9.2';
+        $this->version = '8.0.0';
         $this->author = 'Aplazame SL';
         $this->author_uri = 'https://aplazame.com';
         $this->module_key = '64b13ea3527b4df3fe2e3fc1526ce515';
